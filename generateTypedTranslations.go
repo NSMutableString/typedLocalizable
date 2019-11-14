774b7a54a315ee604d9e92037dbf8be8b93f0e2b
Bram Huenaerts <bram@chronux.be>
2019-11-14 19:48:27 +0100

    read a file line by line

--- generateTypedTranslations.go
+++ generateTypedTranslations.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+)
 
 func main() {
 	fmt.Println("Generate typed translations from Localizable.strings")
+	readFile("./nl.proj/Localizable.strings")
+}
+
+func readFile(filePath string) {
+    file, err := os.Open(filePath)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := scanner.Text()
+        fmt.Println(line)
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
 }
