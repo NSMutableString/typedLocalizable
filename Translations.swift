f09d6c4de3beae67f6fe37a251ca789662e0c323
Bram Huenaerts <bram@chronux.be>
2019-11-15 19:22:10 +0100

    implemented the code generation for Swift

---
+++ Translations.swift
@@ -0,0 +1,15 @@
+//
+//  Translations.swift
+//
+//  Generated code that contains the available keys used in the Localizable.strings
+//  Copyright © 2019. All rights reserved.
+//
+
+public struct Translations {
+	static let generalCancel = "general_cancel"
+	static let generalNext = "general_next"
+	static let generalPrevious = "general_previous"
+	static let generalLoading = "general__loading"
+	static let close = "close"
+	static let pullToRefresh = "pullToRefresh"
+}
--- generateTypedTranslations.go
+++ generateTypedTranslations.go
@@ -7,6 +7,8 @@ import (
     "os"
     "regexp"
     "strings"
+    "bytes"
+    "io/ioutil"
     "github.com/iancoleman/strcase"
 )
 
@@ -22,18 +24,55 @@ func readFile(filePath string) {
     }
     defer file.Close()
 
+    var translationsBuffer bytes.Buffer
+
+    writeHeader(&translationsBuffer)
+    writeContainingStructStart(&translationsBuffer)
+
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
         line := scanner.Text()
-
         match, _ := regexp.MatchString("\"(...+)\" = \"(...+)\";", line)
         if match {
-            s := strings.Split(line, "\"")
-            fmt.Println(strcase.ToLowerCamel(s[1])) // key
+            parts := strings.Split(line, "\"")
+            key := parts[1]
+            propertyName := strcase.ToLowerCamel(key)
+            writeTranslationKeyLine(&translationsBuffer, key, propertyName)
         }
     }
+
+    writeContainingStructEnd(&translationsBuffer)
+
+    fileData := []byte(translationsBuffer.String())
+    ioutil.WriteFile("Translations.swift", fileData, 0644)
 
     if err := scanner.Err(); err != nil {
         log.Fatal(err)
     }
+}
+
+func writeHeader(buffer *bytes.Buffer) {
+    buffer.WriteString("//\n")
+    buffer.WriteString("//  Translations.swift\n")
+    buffer.WriteString("//\n")
+    buffer.WriteString("//  Generated code that contains the available keys used in the Localizable.strings\n")
+    buffer.WriteString("//  Copyright © 2019. All rights reserved.\n")
+    buffer.WriteString("//\n")
+    buffer.WriteString("\n")
+}
+
+func writeTranslationKeyLine(buffer *bytes.Buffer, key string, propertyName string) {
+    buffer.WriteString("\tstatic let " + propertyName + " = \"" +  key + "\"\n")
+}
+
+func writeContainingStruct(buffer *bytes.Buffer, name string) {
+    buffer.WriteString("public struct Translations {\n")
+}
+
+func writeContainingStructStart(buffer *bytes.Buffer) {
+    buffer.WriteString("public struct Translations {\n")
+}
+
+func writeContainingStructEnd(buffer *bytes.Buffer) {
+    buffer.WriteString("}\n")
 }
